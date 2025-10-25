ec7d7e7486ba18db6ecb6329aaa357fd1e1345a4
D4VC3R <davcerval@alu.edu.gva.es>
2025-10-25 22:12:40 +0200

    getUserById añadido

--- src/app/Model/UserModel.php
+++ src/app/Model/UserModel.php
@@ -3,25 +3,41 @@
 namespace App\Model;
 
 use App\Class\User;
+use App\Enum\UserType;
 use Ramsey\Uuid\Uuid;
 
 class UserModel
 {
     public static function getAllUsers(): array{
         $usuario1 = new User(
-            Uuid::uuid4(),
+            Uuid::fromString('550e8400-e29b-41d4-a716-446655440000'),
             "pabloM",
             "molbap",
-            "pablom@mail.com"
+            "pablom@mail.com",
+
         );
         $usuario2 = new User(
-            Uuid::uuid4(),
+            Uuid::fromString('6ba7b810-9dad-11d1-80b4-00c04fd430c8'),
             "pabloC",
             "colbap",
-            "pabloc@mail.com"
+            "pabloc@mail.com",
+            UserType::ADMIN,
+            22
         );
         $usuarios = [$usuario1, $usuario2];
 
         return $usuarios;
+    }
+
+    public static function getUserById(string $uuid): ?User
+    {
+        $usuarios = self::getAllUsers();
+
+        foreach ($usuarios as $usuario) {
+            if ($usuario->getUuid()->toString() === $uuid) {
+                return $usuario;
+            }
+        }
+        return null;
     }
 }
